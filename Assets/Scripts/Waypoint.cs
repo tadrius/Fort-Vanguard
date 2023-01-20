c3ca74b519c9cc6683134d3736811633db6ef616
tadrius <116924580+tadrius@users.noreply.github.com>
2023-01-20 15:23:26 -0600

    Add Buildable Towers
    With Target Locators to aim weapons at Enemy

--- Assets/Scripts/Waypoint.cs
+++ Assets/Scripts/Waypoint.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+
+    [SerializeField] bool isValidSite = true;
+    [SerializeField] GameObject building;
+
+    static RuntimeSpawnsParent runtimeSpawnsParent;
+
+    void Awake() {
+        runtimeSpawnsParent = GameObject.FindObjectOfType<RuntimeSpawnsParent>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void OnMouseDown() {
+        if (isValidSite) {
+            GameObject newBuilding = Instantiate(building, transform.position, Quaternion.identity);
+            newBuilding.transform.parent = runtimeSpawnsParent.transform;
+            isValidSite = false;
+        }
     }
 }
