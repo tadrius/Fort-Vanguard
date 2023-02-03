dabf7ed31cf742ef18cde51bf07db7d3c88a4940
tadrius <116924580+tadrius@users.noreply.github.com>
2023-02-03 16:46:43 -0600

    Add Camera Rotation
    And Move Tiles (such that 0,0 is in bottom left)

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+
+    [SerializeField] float rotationSpeed = 25f;
+
+    Vector3 focalPoint;
+
+    void Awake() {
+        focalPoint = new Vector3(140f, 0f, 60f);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Vector3 rotationVector = new Vector3(rotationSpeed * Time.deltaTime, 0f, 0f);
+        if (Input.GetKey(KeyCode.Q)) {
+            RotateAroundFocalPoint(rotationSpeed * Time.deltaTime);
+        } else if (Input.GetKey(KeyCode.E)) {
+            RotateAroundFocalPoint(-rotationSpeed * Time.deltaTime);
+        }
+    }
+
+    void RotateAroundFocalPoint(float rotation) {
+        transform.RotateAround(focalPoint, Vector3.up, rotation);
+    }
+}
