312c5271ac3e6e651fa3d477e50801c125d88ba9
tadrius <116924580+tadrius@users.noreply.github.com>
2023-03-29 17:27:43 -0500

    Refactor CharacterAnimator
    More accurate method names and current anim list as a field
    Also adjust banner anims

--- Assets/Scripts/Character/CharacterAnimator.cs
+++ Assets/Scripts/Character/CharacterAnimator.cs
@@ -7,6 +7,7 @@ public class CharacterAnimator : MonoBehaviour
 {
 
     [SerializeField] CharacterAnimation currentAnimation;
+    [SerializeField] List<CharacterAnimation> animations;
 
     AnimationRig animationRig;
     AnimationSet animationSet;
@@ -14,17 +15,17 @@ public class CharacterAnimator : MonoBehaviour
     void Awake() {
         animationRig = GetComponent<AnimationRig>();
         animationSet = GetComponentInChildren<AnimationSet>();
+        if (null != animationSet) {
+            LoadAttackAnimations(); // TODO - remove hard-coded animations
+            SetRandomAnimation(animations);
+        }
     }
 
     void Update() {
-        if (null == animationSet && null != currentAnimation) {
-            PlayAnimation();
-        } else {
-            if (null != currentAnimation) {
-                bool animComplete = PlayAnimation();
-                if (animComplete) {
-                    LoadAttackAnimation();
-                }
+        if (null != currentAnimation) {
+            bool animComplete = PlayAnimation();
+            if (animComplete) { // start a different animation from the same list
+                SetRandomAnimation(animations);
             }
         }
     }
@@ -35,14 +36,14 @@ public class CharacterAnimator : MonoBehaviour
         return currentAnimation.PlayAnimation();
     }
 
-    void LoadRandomAnimation(List<CharacterAnimation> animations) {
+    void SetRandomAnimation(List<CharacterAnimation> animations) {
         if (animations.Count > 0) {
             int index = Mathf.RoundToInt(Random.Range(0f, (float) animations.Count - 1));
-            LoadAnimation(animations[index]);
+            SetAnimation(animations[index]);
         }
     }
 
-    void LoadAnimation(CharacterAnimation animation) {
+    void SetAnimation(CharacterAnimation animation) {
         if (currentAnimation != animation) {
             currentAnimation.gameObject.SetActive(false);
         }
@@ -50,24 +51,24 @@ public class CharacterAnimator : MonoBehaviour
         currentAnimation.gameObject.SetActive(true);
     }
 
-    public void LoadWalkAnimation() {
-        LoadRandomAnimation(animationSet.WalkAnimations);
+    public void LoadWalkAnimations() {
+        animations = animationSet.WalkAnimations;
     }
 
-    public void LoadIdleAnimation() {
-        LoadRandomAnimation(animationSet.IdleAnimations);
+    public void LoadIdleAnimations() {
+        animations = animationSet.IdleAnimations;
     }
 
-    public void LoadReloadAnimation() {
-        LoadRandomAnimation(animationSet.ReloadAnimations);
+    public void LoadReloadAnimations() {
+        animations = animationSet.ReloadAnimations;
     }
 
-    public void LoadAttackAnimation() {
-        LoadRandomAnimation(animationSet.AttackAnimations);        
+    public void LoadAttackAnimations() {
+        animations = animationSet.AttackAnimations;
     }
 
-    public void LoadDeathAnimation() {
-        LoadRandomAnimation(animationSet.DeathAnimations);
+    public void LoadDeathAnimations() {
+        animations = animationSet.DeathAnimations;
     }
 
 }
