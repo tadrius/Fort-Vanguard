7f86ff5babd6a4181a27bc679fc505aa6a7904bd
tadrius <116924580+tadrius@users.noreply.github.com>
2023-04-18 11:33:21 -0500

    Disable enemies on reaching end of path
    Also start terrain file for VoxelKnights

--- Assets/Entities/Enemies/Scripts/UnitMover.cs
+++ Assets/Entities/Enemies/Scripts/UnitMover.cs
@@ -73,6 +73,6 @@ public class UnitMover : MonoBehaviour
     void FinishPath() {
         unit.SpawnPenaltyFX();
         unit.IncurPenalty();
-        gameObject.SetActive(false);
+        transform.parent.gameObject.SetActive(false);
     }
 }
