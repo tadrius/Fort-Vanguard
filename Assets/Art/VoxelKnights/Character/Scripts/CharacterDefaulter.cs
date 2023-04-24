a3f4c624faa3232676f56f7d1e9ce516eb347a77
tadrius <116924580+tadrius@users.noreply.github.com>
2023-04-24 14:30:31 -0500

    Helm Edits
    Pikes use heavy parts
    Banners use medium helms but other parts are heavy
    Reimport helms (including crests for medium helms)
    Create and import arrow
    Tweak L Loose R Brace Animation

--- Assets/Art/VoxelKnights/Character/Scripts/CharacterDefaulter.cs
+++ Assets/Art/VoxelKnights/Character/Scripts/CharacterDefaulter.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class CharacterDefaulter : MonoBehaviour
 {
+
+    // To easily create default parts for character prefab variants...
+    // Open the variant, select all parts and revert (in case default were already set)
+    // Exit the prefab and select an instance of the variant.
+    // Reset the Character Defaulter component.
+    // Apply changes to Prefab.
     void Reset() {
         GetComponent<CharacterGenerator>().CreateParts(false);
     }
